11ab8a1f1d1b77e7c3c9da4238335f4b59d3778b
faiq1818 <ghozyerlanggafaiq@gmail.com>
2026-03-30 17:59:53 +0700

    put the initiating code to a function outside of main()

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -26,33 +26,38 @@ import (
 )
 
 func main() {
-	// Graceful shutdown setup
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	// initiate log/slog
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	err := run(logger)
+	if err != nil {
+		logger.Error("Application failed", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run(logger *slog.Logger) error {
+	// This is meant to initialize context.Background() and listen for os.Interrupt
+	// and syscall.SIGTERM. If any signal is caught, the context will notify itself.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
 
 	// get env
 	err := godotenv.Load()
 	if err != nil {
 		logger.Error("Get env failed", "error", err)
-		os.Exit(1)
+		return err
 	}
 
-	// postgres db connection
 	connStr := os.Getenv("DATABASE_URL_CLIENT")
+
+	// open postgres db connection
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		logger.Error("Database open failed", "error", err)
-		os.Exit(1)
+		return err
 	}
 	defer db.Close()
-
-	if err := db.PingContext(ctx); err != nil {
-		logger.Error("Database ping failed", "error", err)
-		os.Exit(1)
-	}
 
 	// Connection pool settings
 	db.SetMaxOpenConns(25)
@@ -66,6 +71,15 @@ func main() {
 		"conn_max_lifetime", 5*time.Minute,
 		"conn_max_idle_time", 1*time.Minute)
 
+	// We need to ping the db connection because sql.Open is Lazy
+	// which means it is just making the db object without opening connection
+	// to physical database
+	err = db.PingContext(ctx)
+	if err != nil {
+		logger.Error("Database ping failed", "error", err)
+		return err
+	}
+
 	// initialize s3
 	cfg, err := config.LoadDefaultConfig(
 		ctx,
@@ -73,7 +87,7 @@ func main() {
 	)
 	if err != nil {
 		logger.Error("Initialize s3 failed", "error", err)
-		os.Exit(1)
+		return err
 	}
 
 	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
@@ -84,7 +98,7 @@ func main() {
 	err = setup.EnsureBucketExists(ctx, s3Client, os.Getenv("S3_BUCKET_NAME"), logger)
 	if err != nil {
 		logger.Error("Checking S3 bucket failed", "error", err)
-		os.Exit(1)
+		return err
 	}
 
 	// s3 manager helper initiate
@@ -110,11 +124,15 @@ func main() {
 		IdleTimeout:  60 * time.Second,
 	}
 
+	// we are using a function inside a goroutine to execute srv.ListenAndServe
+	// so the main thread only waits for a graceful shutdown signal from the
+	// srv.ListenAndServe running in the other goroutine thread
 	go func() {
 		logger.Info("Server starting on port " + port)
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
 			logger.Error("Server failed", "error", err)
-			os.Exit(1)
 		}
 	}()
 
@@ -124,9 +142,12 @@ func main() {
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	if err := srv.Shutdown(shutdownCtx); err != nil {
+	err = srv.Shutdown(shutdownCtx)
+	if err != nil {
 		logger.Error("Server shutdown failed", "error", err)
 	}
 
 	logger.Info("Server stopped gracefully")
+
+	return nil
 }
